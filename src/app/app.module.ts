d4bb49840c9019f05ebc2c7d5e130f014f68ff7d
Luan <luan_cherubim@hotmail.com>
2019-08-31 14:57:40 -0300

    Fix App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgencySearchPageComponent } from './agency-search-page/agency-search-page.component';
 import { AgencyListPageComponent } from './agency-list-page/agency-list-page.component';
 import { AgencyDetailPageComponent } from './agency-detail-page/agency-detail-page.component';
+import { ActivityIndicatorComponent } from './activity-indicator/activity-indicator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AgencySearchPageComponent,
     AgencyListPageComponent,
-    AgencyDetailPageComponent
+    AgencyDetailPageComponent,
+    ActivityIndicatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
