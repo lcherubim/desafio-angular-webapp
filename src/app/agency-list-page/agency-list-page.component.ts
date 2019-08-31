b91e3f7d232d8fb140e99aab86caccb748911f21
Luan <luan_cherubim@hotmail.com>
2019-08-31 14:54:11 -0300

    Add content to agency-list-page component

--- src/app/agency-list-page/agency-list-page.component.ts
+++ src/app/agency-list-page/agency-list-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Agency } from './../models/agency.model';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { Agency } from '../models/agency.model';
 import { AgencyService } from '../services/agency.service';
+
+declare var google;
 
 @Component({
   selector: 'app-agency-list-page',
@@ -11,16 +13,31 @@ import { AgencyService } from '../services/agency.service';
 })
 export class AgencyListPageComponent implements OnInit {
 
-  public agencyList: Array<Agency>;
+  @ViewChild('map') mapElement: ElementRef;
+
+  public agencyList: Array<any>;
+
+  public isLoading = false;
 
   public showDetail(agency: Agency) {
-    this.agency.select(agency);
-    this.router.navigate(['detalhe']);
+    this.isLoading = true;
+    const request = {
+      placeId: agency.place_id,
+      fields: ['name', 'formatted_phone_number', 'opening_hours']
+    };
+    const map = new google.maps.Map(this.mapElement.nativeElement);
+    const service = new google.maps.places.PlacesService(map);
+    service.getDetails(request, (result) => {
+      this.agency.selectedAgency = result;
+      this.ngZone.run(() => this.router.navigate(['detalhe'])).then();
+      this.isLoading = false;
+    });
   }
 
   constructor(
     private router: Router,
-    private agency: AgencyService) { }
+    private agency: AgencyService,
+    private ngZone: NgZone) { }
 
   ngOnInit() {
     if (this.agency.agencyList.length > 0) {
