38be807fed5cfa245673f7810498209124647ae1
Luan <luan_cherubim@hotmail.com>
2019-08-31 14:57:29 -0300

    Add agency service

--- src/app/services/agency.service.ts
+++ src/app/services/agency.service.ts
@@ -1,30 +1,20 @@
-import { Agency } from './../models/agency.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Agency, AgencyDetail } from '../models/agency.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
 
-  public selectedAgency = new Agency();
+  public selectedAgency = new AgencyDetail();
 
-  public agencyList = new Array<Agency>();
+  public agencyList = new Array<any>();
 
-  public search() {
-    this.agencyList = [
-      { name: 'Agencia #01' },
-      { name: 'Agencia #02' },
-      { name: 'Agencia #03' },
-      { name: 'Agencia #04' },
-      { name: 'Agencia #05' },
-      { name: 'Agencia #06' },
-      { name: 'Agencia #07' },
-      { name: 'Agencia #08' },
-    ];
-  }
-
-  public select(agency: Agency) {
-    this.selectedAgency = agency;
+  public select(agencyDetail: AgencyDetail) {
+    this.selectedAgency = agencyDetail;
   }
 
   constructor() { }
