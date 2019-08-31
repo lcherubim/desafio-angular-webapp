fb35b4ac4ae51f731fb0b043a823129febc417c0
Luan <luan_cherubim@hotmail.com>
2019-08-30 21:54:01 -0300

    Add Routes Config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AgencyListPageComponent } from './agency-list-page/agency-list-page.component';
+import { AgencySearchPageComponent } from './agency-search-page/agency-search-page.component';
+import { AgencyDetailPageComponent } from './agency-detail-page/agency-detail-page.component';
+
+const routes: Routes = [
+  {
+    path: 'agencias',
+    component: AgencyListPageComponent
+  },
+  {
+    path: 'detalhe',
+    component: AgencyDetailPageComponent
+  },
+  {
+    path: '',
+    component: AgencySearchPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
