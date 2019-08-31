d85e0404642f07119cb4492c7d3df6943c349901
Luan <luan_cherubim@hotmail.com>
2019-08-30 21:58:40 -0300

    Add initial content to agency-list-page component

--- src/app/agency-list-page/agency-list-page.component.ts
+++ src/app/agency-list-page/agency-list-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Agency } from './../models/agency.model';
+import { Router } from '@angular/router';
+import { AgencyService } from '../services/agency.service';
+
 @Component({
   selector: 'app-agency-list-page',
   templateUrl: './agency-list-page.component.html',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgencyListPageComponent implements OnInit {
 
-  constructor() { }
+  public agencyList: Array<Agency>;
+
+  public showDetail(agency: Agency) {
+    this.agency.select(agency);
+    this.router.navigate(['detalhe']);
+  }
+
+  constructor(
+    private router: Router,
+    private agency: AgencyService) { }
 
   ngOnInit() {
+    if (this.agency.agencyList.length > 0) {
+      this.agencyList = this.agency.agencyList;
+    } else {
+      this.router.navigate(['']);
+    }
   }
 
 }
