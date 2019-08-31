d27416a0261c9fdb20e6d63634c33adab5554aa2
Luan <luan_cherubim@hotmail.com>
2019-08-31 14:58:17 -0300

    Add get user position to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { LocationService } from './services/location.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'agency-finder';
+export class AppComponent implements OnInit {
+
+  constructor(private get: LocationService) {}
+
+  ngOnInit() {
+    this.get.getUserPosition();
+  }
 }
+
