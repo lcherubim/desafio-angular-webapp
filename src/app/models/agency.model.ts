5b397fb696f92445cce241c0774ac7a2cfd9f074
Luan <luan_cherubim@hotmail.com>
2019-08-31 14:55:29 -0300

    Fix agency models

--- src/app/models/agency.model.ts
+++ src/app/models/agency.model.ts
@@ -1,3 +1,12 @@
 export class Agency {
     name: string;
+    place_id: string;
+}
+
+export class AgencyDetail {
+    name: string;
+    formatted_phone_number: string;
+    opening_hours: {
+        weekday_text: Array<string>
+    };
 }
