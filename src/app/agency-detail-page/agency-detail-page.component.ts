a808369d2d0dc6d7e2c5ed8d730ee75564b64880
Luan <luan_cherubim@hotmail.com>
2019-09-01 13:45:13 -0300

    Add Register Access Data on agency-detail-page component

--- src/app/agency-detail-page/agency-detail-page.component.ts
+++ src/app/agency-detail-page/agency-detail-page.component.ts
@@ -1,8 +1,11 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { AgencyDetail } from './../models/agency.model';
 import { AgencyService } from './../services/agency.service';
+import { UserDeviceService } from './../services/user-device.service';
+import { LocationService } from './../services/location.service';
 
 @Component({
   selector: 'app-agency-detail-page',
@@ -17,7 +20,7 @@ export class AgencyDetailPageComponent implements OnInit {
     name: null,
     formatted_phone_number: null,
     opening_hours: {
-        weekday_text: [null, null, null, null, null, null, null]
+      weekday_text: [null, null, null, null, null, null, null]
     }
   };
 
@@ -25,14 +28,38 @@ export class AgencyDetailPageComponent implements OnInit {
     this.router.navigate(['agencias']);
   }
 
+  public registerAccessData() {
+    const url = 'http://localhost:3000/registros';
+    const body = {
+      Id: 0,
+      Latitude: this.user.userPosition.coords.latitude,
+      Longitude: this.user.userPosition.coords.latitude,
+      Device: this.device.userDeviceInfo.device,
+      Browser: this.device.userDeviceInfo.browser,
+      Number: this.agency.selectedAgency.formatted_phone_number
+    };
+    const httpOptions = {
+      headers: new HttpHeaders({
+        ContentType: 'application/json'
+      }),
+      responseType: 'text' as 'json'
+    };
+    return this.httpClient.post(url, body, httpOptions);
+  }
+
   constructor(
     private agency: AgencyService,
-    private router: Router
+    private router: Router,
+    private httpClient: HttpClient,
+    private user: LocationService,
+    private device: UserDeviceService
   ) { }
 
   ngOnInit() {
     if (this.agency.selectedAgency.constructor === Object) {
       this.selectedAgency = this.agency.selectedAgency;
+      this.registerAccessData().subscribe((res) => {
+      }, error => console.log(error));
     } else {
       this.router.navigate(['']);
     }
